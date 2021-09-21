d7f7560d281181b8c82459bd96060d205411d847
Eric <eric@naguras.com>
2021-09-21 16:38:24 +0700

    EC2 address changed

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -14,19 +14,19 @@ echo "You are on the clean master branch, proceeding with deployment"
 
 
 
-ssh ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com "mkdir -p /home/ubuntu/projects/crm-prod"
-rsync -avz -e 'ssh' src ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
-rsync -avz -e 'ssh' package.json ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
-rsync -avz -e 'ssh' package-lock.json ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
-# rsync -avz -e 'ssh' pm2.json ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
-rsync -avz -e 'ssh' Dockerfile ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
-rsync -avz -e 'ssh' docker-compose.yml ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu
+ssh ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com "mkdir -p /home/ubuntu/projects/crm-prod"
+rsync -avz -e 'ssh' src ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
+rsync -avz -e 'ssh' package.json ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
+rsync -avz -e 'ssh' package-lock.json ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
+# rsync -avz -e 'ssh' pm2.json ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
+rsync -avz -e 'ssh' Dockerfile ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
+rsync -avz -e 'ssh' docker-compose.yml ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com:/home/ubuntu
 
 # Build dockerfile
-ssh ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com "cd /home/ubuntu/projects/crm-prod && docker build --no-cache -t crm/api-prod:latest ."
+ssh ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com "cd /home/ubuntu/projects/crm-prod && docker build --no-cache -t crm/api-prod:latest ."
 # Start the new container
-ssh ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com "cd /home/ubuntu && docker-compose up -d"
+ssh ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com "cd /home/ubuntu && docker-compose up -d"
 # clean up loose containers, images
-ssh ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com "docker system prune -f"
+ssh ubuntu@ec2-18-170-230-185.eu-west-2.compute.amazonaws.com "docker system prune -f"
 echo
 echo "Production server is ready."
