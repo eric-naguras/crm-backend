91ba9fd5240371194cce0adaeeba773f356bc2cd
Eric <eric@naguras.com>
2021-09-21 16:25:19 +0700

    removed pm2 from dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:14
 WORKDIR /usr/src/app
-RUN npm install pm2 -g
-RUN pm2 install profiler
+# RUN npm install pm2 -g
+# RUN pm2 install profiler
 
 
 COPY package.json .
@@ -9,6 +9,6 @@ COPY package-lock.json .
 
 RUN npm i --only=prod
 
-COPY pm2.json ./
+# COPY pm2.json ./
 
-CMD ["pm2-runtime", "pm2.json"]
+CMD ["node", "src/index.js"]
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -18,7 +18,7 @@ ssh ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com "mkdir -p /home/ub
 rsync -avz -e 'ssh' src ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
 rsync -avz -e 'ssh' package.json ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
 rsync -avz -e 'ssh' package-lock.json ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
-rsync -avz -e 'ssh' pm2.json ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
+# rsync -avz -e 'ssh' pm2.json ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
 rsync -avz -e 'ssh' Dockerfile ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu/projects/crm-prod
 rsync -avz -e 'ssh' docker-compose.yml ubuntu@ec2-35-178-187-243.eu-west-2.compute.amazonaws.com:/home/ubuntu
 
